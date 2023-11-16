2a34d18960d1734dfa98d082d5863f600eabec33
Thomas Mol <email@thomasmol.com>
2023-11-16 13:37:20 +0100

    Update compiledocs.py to output concatenated files to a specific directory

--- compiledocs.py
+++ compiledocs.py
@@ -1,6 +1,6 @@
 import os
 
-def concatenate_mdx_files(dir_names, output_dir="compiled"):
+def concatenate_mdx_files(dir_names, output_dir="compiled/mintlify"):
     for dir_name in dir_names:
         # Prepare the content to be written in the new file
         concatenated_content = ""
@@ -10,6 +10,9 @@ def concatenate_mdx_files(dir_names, output_dir="compiled"):
             print(f"Directory not found: {dir_name}")
             continue
 
+        # Get the base name of the directory
+        dir_base_name = os.path.basename(dir_name)
+
         # List all files in the directory
         for filename in os.listdir(dir_name):
             if filename.endswith('.mdx'):
@@ -18,11 +21,11 @@ def concatenate_mdx_files(dir_names, output_dir="compiled"):
                     concatenated_content += file.read() + "\n"
 
         # Create a new .mdx file with the concatenated content
-        with open(os.path.join(output_dir, f"{dir_name}.mdx"), 'w') as new_file:
+        with open(os.path.join(output_dir, f"{dir_base_name}.mdx"), 'w') as new_file:
             new_file.write(concatenated_content)
 
         print(f"Concatenated file created for directory: {dir_name}")
 
 # Example usage
-dir_names = ["components", "tutorials", "getstarted", "features"]  # Replace with your directory names
+dir_names = ["docs-main/content/components"]  # Replace with your directory names
 concatenate_mdx_files(dir_names)
